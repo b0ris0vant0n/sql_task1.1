984e6c758fceca862f44d1a392ece49d2e3738c7
Anton Borisov <borisov2011@gmail.com>
2022-07-08 11:09:24 +0300

    second commit

--- Script-1.sql
+++ Script-1.sql
@@ -1,39 +1,46 @@
 CREATE TABLE IF NOT EXISTS genre(
-genre_id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 name VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS singer(
-singer_id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 name VARCHAR(60) NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS singer_genre(
-singer_id int REFERENCES singer(singer_id),
-genre_id int REFERENCES genre(genre_id)
+singer_id int REFERENCES singer(id),
+genre_id int REFERENCES genre(id)
 );
 
 CREATE TABLE IF NOT EXISTS album(
-album_id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 name VARCHAR(60) NOT NULL,
 release_year int NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS album_singer(
-singer_id int REFERENCES singer(singer_id),
-album_id int REFERENCES album(album_id)
+singer_id int REFERENCES singer(id),
+album_id int REFERENCES album(id),
+CONSTRAINT pk PRIMARY KEY (singer_id, album_id)
 );
 
 CREATE TABLE IF NOT EXISTS track(
-track_id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 name varchar(80) NOT NULL,
-album_id int REFERENCES album(album_id)
+album_id int REFERENCES album(id)
 );
 
 CREATE TABLE IF NOT EXISTS collections(
-collections_id SERIAL PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 release_year int NOT NULL,
-name varchar(60) NOT NULL,
-album_id int REFERENCES album(album_id),
-track_id int REFERENCES track(track_id)
+name varchar(60) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS track_collections(
+collections_id int REFERENCES collections(id),
+track_id int REFERENCES track(id),
+CONSTRAINT pk_track PRIMARY KEY (collections_id, track_id)
+);
+
 
